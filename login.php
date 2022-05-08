2d2be17ae9b3f8f5808347006ded839bc178bd42
NirupamBitsPilani <100022827+NirupamBitsPilani@users.noreply.github.com>
2022-05-08 17:26:18 +0530

    Update login.php

--- login.php
+++ login.php
@@ -26,7 +26,7 @@
             header("Location: index.php");
         } else {
             echo "<div class='form'>
-                  <h3>Incorrect Username/password.</h3><br/>
+                  <h3>Invalid Username or incorrect password.</h3><br/>
                   <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
                   </div>";
         }
